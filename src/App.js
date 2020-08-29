0486c325e73a4c6c52af93ffbf5c961bfb72ec10
Marlon Mitchell <mdmitch7@outlook.com>
2020-08-28 20:28:24 -0400

    34. Home & NotFound
    created a Home and NotFound page, then modified App.js for implimentation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/alertState';
-
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 
 const App = () => {
     return (
@@ -22,18 +21,12 @@ const App = () => {
                         <Alert/>
                         <Switch>
                             <Route exact path='/'
-                                render={
-                                    props => (
-                                        <Fragment>
-                                            <Search/>
-                                            <Users/>
-                                        </Fragment>
-                                    )
-                                }/>
+                                component={Home}/>
                             <Route exact path='/about'
                                 component={About}/>
                             <Route exact path='/user/:login'
                                 component={User}/>
+                            <Route component={NotFound}/>
                         </Switch>
                     </div>
                 </div>
